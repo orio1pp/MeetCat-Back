1268ca621ab7807580b467c1bc22a4b233d15eff
Yavanna92 <txellguevara.1905@gmail.com>
2022-11-20 16:33:45 +0100

    Add pagination to events

--- src/main/kotlin/upc/fib/pes/grup121/repository/EventRepository.kt
+++ src/main/kotlin/upc/fib/pes/grup121/repository/EventRepository.kt
@@ -2,11 +2,13 @@ package upc.fib.pes.grup121.repository
 
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import upc.fib.pes.grup121.model.Event
+import java.awt.print.Pageable
 
 @Repository
-interface EventRepository : CrudRepository<Event, Long>{
+interface EventRepository : CrudRepository<Event, Long>, PagingAndSortingRepository<Event, Long> {
 
     fun existsByAgendaEventCode(agendaEventCode: Long?): Boolean
 
