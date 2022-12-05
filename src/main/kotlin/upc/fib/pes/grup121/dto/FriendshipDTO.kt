731e38eacf4e91e99f7121034c98b80d89b5edbe
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-12-05 23:40:14 +0100

    Now you can have friends, getchats

--- src/main/kotlin/upc/fib/pes/grup121/dto/FriendshipDTO.kt
+++ src/main/kotlin/upc/fib/pes/grup121/dto/FriendshipDTO.kt
@@ -1,7 +1,6 @@
 package upc.fib.pes.grup121.dto
 
 data class FriendshipDTO(
-    var id: Long,
     var friendId: String,
     var ownerId: String
 ) {
--- src/main/kotlin/upc/fib/pes/grup121/service/FriendshipService.kt
+++ src/main/kotlin/upc/fib/pes/grup121/service/FriendshipService.kt
@@ -1,23 +1,39 @@
 package upc.fib.pes.grup121.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
 import upc.fib.pes.grup121.dto.FriendshipDTO
+import java.lang.Exception
 
 
 @Service
 class FriendshipService(
-    private final val restTemplate: RestTemplate = RestTemplate()
+    private final val userService: UserService
 ) {
-    var urlChats: String = "http://localhost:8081/friendship";
-    fun getFriendshipsbyUsername(userNameOnwer: String): List<FriendshipDTO>?{
-        var result:List<FriendshipDTO>? = restTemplate.getForObject<List<FriendshipDTO>>(urlChats + "?username=" + userNameOnwer);
+    private final val restTemplate: RestTemplate = RestTemplate()
+    var urlChats: String = "http://localhost:7070/friendship";
+    fun getFriendshipsbyUsername(userNameOnwer: String): List<FriendshipDTO>? {
+        var result: List<FriendshipDTO>? =
+            restTemplate.getForObject<List<FriendshipDTO>>(urlChats + "?username=" + userNameOnwer);
         return result;
     }
-    fun insertFriendship(friendship: FriendshipDTO) {
-        val request = HttpEntity(friendship)
-        restTemplate?.postForLocation(urlChats, request);
+
+    fun insertFriendship(username: String): FriendshipDTO? {
+        try {
+            userService.getByUsername(username).let {
+                val ownerId: String = SecurityContextHolder.getContext().authentication.name;
+                val friendShip: FriendshipDTO = FriendshipDTO(username, ownerId)
+                val request = HttpEntity(friendShip)
+                restTemplate?.postForLocation(urlChats, request)
+                return friendShip;
+            }
+            return null;
+        } catch (e: Exception) {
+            return null;
+        }
     }
 }
