d3e4c11b9304e971ebb398b75ed701bcd2d81325
AlejandroLozanoAparicio <alejandro.lozano.aparicio@estudiantat.upc.edu>
2023-01-13 02:40:37 +0100

    a

--- src/main/kotlin/upc/fib/pes/grup121/security/SecurityConfig.kt
+++ src/main/kotlin/upc/fib/pes/grup121/security/SecurityConfig.kt
@@ -32,8 +32,8 @@ class SecurityConfig(
     override fun configure(http: HttpSecurity) {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http.authorizeRequests().antMatchers("/reported").hasRole("admin")
-        http.authorizeRequests().antMatchers("/{id}/unreport").hasRole("admin")
+        http.authorizeRequests().antMatchers("/events/reported").hasRole("admin")
+        http.authorizeRequests().antMatchers("/events/{id}/unreport").hasRole("admin")
         http.authorizeRequests().antMatchers("/login", "/users/refresh/token", "/**").permitAll()
         http.authorizeRequests().anyRequest().authenticated()
         http.addFilter(CustomAuthenticationFilter(authenticationManagerBean()))
